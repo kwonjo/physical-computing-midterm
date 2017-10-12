9e57dbaf227237cbe77cb660df3e9c1694547eaf
Jo Kwon <31631968+kwonjo@users.noreply.github.com>
2017-10-11 20:14:08 -0400

    Update photon.ino

--- photon.ino
+++ photon.ino
@@ -1,58 +1,40 @@
 
-//pin definitions
 int buttonPin1 = D2;
 int buttonPin2 = D3;
-int LEDPin2 = D4;//Yuchen's LED
-void lightup(const char *, const char *);
-
-
 int val1 = 0;//int pin1State = LOW;
 int val2 = 0;//int pin2State = LOW;
-
 //setup
 void setup() {
   pinMode(buttonPin1, INPUT_PULLUP);//pullup resistor in spark
   pinMode(buttonPin2, INPUT_PULLUP);
-  pinMode(LEDPin2, OUTPUT);
-  Particle.subscribe("pika", lightup, MY_DEVICES); //communicate with Yuchen's photon
-  
   Serial.begin(9600);
+  Particle.subscribe("pikafeed", feed,"430053001151353338363333"); //communicate with Yuchen's photon
 }
 // the loop routine runs over and over again forever
 void loop() {
-
   val1 = digitalRead(buttonPin1);
   val2 = digitalRead(buttonPin2);
-
   if((val1 == LOW) && (val2 == LOW)){
-    Spark.publish("pikachu_status","Pikachu is eating too much!", 60, PRIVATE);
+    Spark.publish("pikachu_status","Pikachu is eating too much!", 60);
     delay(5000); 
     }//buttonPin1&2 detected at the sametime 
   else if((val1 == LOW) && (val2 == HIGH)){
-  	Spark.publish("pikachu_status", "Pikachu is HAPPY because you are healthy!", 60, PRIVATE);
+    Spark.publish("pikachu_status", "Pikachu is HAPPY because you are healthy!", 60);
     delay(5000); 
-  	} //buttonPin1 detects "healthy food"
+    Particle.publish("pika", "430053001151353338363333"); 
+    } //buttonPin1 detects "healthy food" so LEDPin2 is "light"
   else if((val2 == LOW) && (val1 == HIGH)){
-   	Spark.publish("pikachu_status", "Pikachu is SAD because you are eating junk food", 60, PRIVATE);
-   	delay(5000); 
-   	}//buttonPin2 detects "unhealthy food"
+    Spark.publish("pikachu_status", "Pikachu is SAD because you are eating junk food", 60);
+    Particle.publish("pika2", "430053001151353338363333"); //communicate with Yuchen's photon
+    delay(5000); 
+    }//buttonPin2 detects "unhealthy food"
   else {
-    Particle.publish("pika", "light", PRIVATE);
-    delay(5000);//nothing is pushed so LEDPin2 is "light"
+    delay(5000);//nothing happens
    }
   
   val1 = 0;
   val2 = 0;
-
 }
-
-void lightup(const char *event, const char *data) {
-    
-  if(strcmp(data,"light")==0){
-    digitalWrite(LEDPin2, HIGH);//LEDPin2 is on
-  }
-  else{
-    digitalWrite(LEDPin2, LOW);//LEDPin2 is off
-  }
+void feed(const char *event, const char *data) {
+    Spark.publish("pikachu_feed", "Good Morning, Pikachu needs to EAT!", 60);
 }
-
